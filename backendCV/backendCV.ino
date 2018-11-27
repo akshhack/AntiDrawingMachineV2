d6024cd8683293f4680ac3ab806e0c6c677bd729
akshhack <akshatp@andrew.cmu.edu>
2018-11-27 16:09:06 -0500

    changed prevPositions oto array of structs than an array of pointers. Changed and modified determine direction code (credits: Harsh). Harsh and I also played around with the constants to see some behavior from the bot

--- backendCV/backendCV.ino
+++ backendCV/backendCV.ino
@@ -1,8 +1,9 @@
 // POSITION FACTORS - DETERMINE THESE
-#define X_DISTANCE_FACTOR 1.0
-#define Y_DISTANCE_FACTOR 1.5
+#define X_DISTANCE_FACTOR 1.0*3.33
+#define Y_DISTANCE_FACTOR 2.5*5
 
 // SPEED TOLERANCE
+#define SPEED_MINIMUM 0.02
 #define SPEED_THRESHOLD_UPPER 0.5 
 #define SPEED_THRESHOLD_LOWER 0.1
 
@@ -17,9 +18,9 @@
 #define BAUD_RATE 115200
 
 // QUEUE SPECS
-#define QUEUE_SIZE 10
+#define QUEUE_SIZE 100
 
-#define DELAY 10000
+#define DELAY 0
 
 #include <Pixy2.h>
 
@@ -39,7 +40,7 @@ typedef struct {
   PenPosition* pos;
   PenVelocity* vel;
   
-  PenPosition* prevPositions[QUEUE_SIZE];
+  PenPosition prevPositions[QUEUE_SIZE];
 } PenMotion;
 
 typedef struct {
@@ -51,22 +52,9 @@ typedef struct {
 } PenState;
 
 PenPosition posInit = {0, 0};
-PenPosition posInit1 = {0, 0};
-PenPosition posInit2 = {0, 0};
-PenPosition posInit3 = {0, 0};
-PenPosition posInit4 = {0, 0};
-PenPosition posInit5 = {0, 0};
-PenPosition posInit6 = {0, 0};
-PenPosition posInit7 = {0, 0};
-PenPosition posInit8 = {0, 0};
-PenPosition posInit9 = {0, 0};
-PenPosition posInit10 = {0, 0};
 
 PenVelocity velInit = {0.0, 0.0};
-PenMotion penMotion = {&posInit, &velInit, {&posInit1, &posInit2, &posInit3,
-                                            &posInit4, &posInit5, &posInit6,
-                                            &posInit7, &posInit8, &posInit9,
-                                            &posInit10}};
+PenMotion penMotion = {&posInit, &velInit};
 PenState penState = {false, false, false, false, 'N'};
 int setupCounter = 0;
 int moveCounter = 0;
@@ -82,11 +70,11 @@ void resetPenState() {
 }
 
 void decideHorizontal() {
-  uint16_t firstPosition = penMotion.prevPositions[0]->y;
+  uint16_t firstPosition = penMotion.prevPositions[0].y;
   bool isHorizontal = true;
   // check if all positions are in tolerance of the firstPosition
   for (int i = 0; i < QUEUE_SIZE; i += 1) {
-    uint16_t currentPos = penMotion.prevPositions[i]->y;
+    uint16_t currentPos = penMotion.prevPositions[i].y;
     if (currentPos < firstPosition - Y_TOLERANCE || 
         currentPos > firstPosition + Y_TOLERANCE) {
       isHorizontal = false;
@@ -97,11 +85,11 @@ void decideHorizontal() {
 }
 
 void decideVertical() {
-  uint16_t firstPosition = penMotion.prevPositions[0]->x;
+  uint16_t firstPosition = penMotion.prevPositions[0].x;
   bool isVertical = true;
   // check if all positions are in tolerance of the firstPosition
   for (int i = 0; i < QUEUE_SIZE; i += 1) {
-    uint16_t currentPos = penMotion.prevPositions[i]->x;
+    uint16_t currentPos = penMotion.prevPositions[i].x;
     if (currentPos < firstPosition - X_TOLERANCE || 
         currentPos > firstPosition + X_TOLERANCE) {
       isVertical = false;
@@ -124,45 +112,48 @@ void decideSlow() {
 }
 
 void decideDirection() {
+
+  Serial.println("Deciding direction");
+
+  Serial.print("Velocity Y component: ");
+  Serial.println(penMotion.vel->vy);
+  Serial.print("Velocity X component: ");
+  Serial.println(penMotion.vel->vx);
   
   double vy_abs = abs(penMotion.vel->vy);
   double vx_abs = abs(penMotion.vel->vx);
+
   
-  if (penMotion.vel->vx > 0 && penMotion.vel->vy > 0) {
-    // NE  
-    if (vy_abs > vx_abs) {
-      penState.dir = 'N';  
-    } else {
-        penState.dir = 'E';  
-    }
-  } else if (penMotion.vel->vx > 0 && penMotion.vel->vy < 0) {
-    // SE
-    if (vy_abs > vx_abs) {
+  Serial.print("Velocity Y component (ABS): ");
+  Serial.println(vy_abs);
+  Serial.print("Velocity X component (ABS): ");
+  Serial.println(vx_abs);
+
+  if (vy_abs > vx_abs) {
+    if (penMotion.vel->vy <= 0.0) {
       penState.dir = 'S';  
+    } else if (penMotion.vel->vy > 0.0){
+        penState.dir = 'N';  
     } else {
-        penState.dir = 'E';  
-    }
-  } else if (penMotion.vel->vx < 0 && penMotion.vel->vy > 0) {
-    // NW
-    if (vy_abs > vx_abs) {
-      penState.dir = 'N';  
-    } else {
-        penState.dir = 'W';  
-    }
+        Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (Y)"); 
+     }
+  } else if (vx_abs > vy_abs) {
+       if (penMotion.vel->vx <= 0.0) {
+          penState.dir = 'E';
+       } else if (penMotion.vel->vx > 0.0) {
+          penState.dir = 'W';   
+       } else {
+            Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (X)"); 
+       }
   } else {
-    // SW
-    if (vy_abs > vx_abs) {
-      penState.dir = 'S';  
-    } else {
-        penState.dir = 'W';
-    }
+      Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (ABS)");  
   }
 }
 
 void frameListSetup(uint16_t x, uint16_t y) {
   if (setupCounter < QUEUE_SIZE) {
-    penMotion.prevPositions[setupCounter]->x = x;
-    penMotion.prevPositions[setupCounter]->y = y;
+    penMotion.prevPositions[setupCounter].x = x;
+    penMotion.prevPositions[setupCounter].y = y;
     setupCounter += 1; 
   }
 }
@@ -172,33 +163,33 @@ void frameListSetup(uint16_t x, uint16_t y) {
  */
 void addBlockToFrameList(uint16_t x, uint16_t y) {
     for (int i = 0; i < QUEUE_SIZE - 1; i += 1) {
-      penMotion.prevPositions[i]->x = penMotion.prevPositions[i+1]->x;
-      penMotion.prevPositions[i]->y = penMotion.prevPositions[i+1]->y;
+      penMotion.prevPositions[i].x = penMotion.prevPositions[i+1].x;
+      penMotion.prevPositions[i].y = penMotion.prevPositions[i+1].y;
     }
 
-    penMotion.prevPositions[QUEUE_SIZE - 1]->x = x;
-    penMotion.prevPositions[QUEUE_SIZE - 1]->y = y; 
+    penMotion.prevPositions[QUEUE_SIZE - 1].x = x;
+    penMotion.prevPositions[QUEUE_SIZE - 1].y = y; 
 }
 
 void updatePenPosition() {
-  uint16_t x = penMotion.prevPositions[QUEUE_SIZE - 1]->x;
-  uint16_t y = penMotion.prevPositions[QUEUE_SIZE - 1]->y;
+  uint16_t x = penMotion.prevPositions[QUEUE_SIZE - 1].x;
+  uint16_t y = penMotion.prevPositions[QUEUE_SIZE - 1].y;
 
   penMotion.pos->x = x;
   penMotion.pos->y = y;
 }
 
 void updatePenVelocities() {
-  uint16_t mostRecentX = penMotion.prevPositions[QUEUE_SIZE - 1]->x;
-  uint16_t mostRecentY = penMotion.prevPositions[QUEUE_SIZE - 1]->y;
+  uint16_t mostRecentX = penMotion.prevPositions[QUEUE_SIZE - 1].x;
+  uint16_t mostRecentY = penMotion.prevPositions[QUEUE_SIZE - 1].y;
 
   double velocitySumX = 0.0;
   double velocitySumY = 0.0;
 
   // get average of all velocities
   for (int i = QUEUE_SIZE - 2; i >= 0; i -= 1) {
-    uint16_t x = penMotion.prevPositions[i]->x;
-    uint16_t y = penMotion.prevPositions[i]->y; 
+    uint16_t x = penMotion.prevPositions[i].x;
+    uint16_t y = penMotion.prevPositions[i].y; 
 
     double velocityX = (mostRecentX * 1.0 - x) / ((QUEUE_SIZE - 1 - i) * FRAME_TIME);
     double velocityY = (mostRecentY * 1.0 - y) / ((QUEUE_SIZE - 1 - i) * FRAME_TIME);
@@ -210,11 +201,19 @@ void updatePenVelocities() {
   penMotion.vel->vx = velocitySumX / (QUEUE_SIZE - 1);
   penMotion.vel->vy = velocitySumY / (QUEUE_SIZE - 1);
 
-  Serial.print("Velocity X component: ");
-  Serial.println(penMotion.vel->vx);
+//  if (abs(penMotion.vel->vx) < SPEED_MINIMUM) {
+//    penMotion.vel->vx = 0.0;  
+//  }
+//
+//  if (abs(penMotion.vel->vy < SPEED_MINIMUM)) {
+//    penMotion.vel->vy = 0.0;  
+//  }
 
-  Serial.print("Velocity Y component: ");
-  Serial.println(penMotion.vel->vy);
+//  Serial.print("Velocity X component: ");
+//  Serial.println(penMotion.vel->vx);
+//
+//  Serial.print("Velocity Y component: ");
+//  Serial.println(penMotion.vel->vy);
 }
 
 void decideMove() {
@@ -315,9 +314,9 @@ void printFrameList() {
     Serial.println(i + 1);
 
     Serial.print("X position: ");
-    Serial.println(penMotion.prevPositions[i]->x);
+    Serial.println(penMotion.prevPositions[i].x);
     Serial.print("Y position: ");
-    Serial.println(penMotion.prevPositions[i]->y);
+    Serial.println(penMotion.prevPositions[i].y);
   }  
 }
 
@@ -332,25 +331,22 @@ void loop() {
     uint16_t y = pixy.ccc.blocks[0].m_y * Y_DISTANCE_FACTOR;
 
 
-    Serial.print("X value = ");
-    Serial.println(pixy.ccc.blocks[0].m_x); // only take most relevant block
-    Serial.print("Y value = ");
-    Serial.println(pixy.ccc.blocks[0].m_y);
+//    Serial.print("X value = ");
+//    Serial.println(pixy.ccc.blocks[0].m_x); // only take most relevant block
+//    Serial.print("Y value = ");
+//    Serial.println(pixy.ccc.blocks[0].m_y);
 
     if (setupCounter < QUEUE_SIZE) {
-      //Serial.println("Setting up");
       frameListSetup(x, y);  
     } else {
-        //Serial.println("Set up complete");
         addBlockToFrameList(x, y);
-
-        //printFrameList();
   
         updatePenPosition();
       
         updatePenVelocities();
 
         if (moveCounter % QUEUE_SIZE == 0) {
+          //printFrameList();
           decideMove();
           moveCounter = 0; 
           delay(DELAY); 
--- integration/integration.ino
+++ integration/integration.ino
@@ -8,31 +8,36 @@
 
 #define MAX_SPEED 8000
 #define ACCELERATION 2750
-#define BAUD_RATE 115200
 
-#define SKID_TIMES 60
-#define DELAY 300
+#define SKID_TIMES 8
 
 // POSITION FACTORS - DETERMINE THESE
-#define X_DISTANCE_FACTOR 1.0
-#define Y_DISTANCE_FACTOR 3.75
+#define X_DISTANCE_FACTOR 1.0*3.33
+#define Y_DISTANCE_FACTOR 2.5*7 // I changed this from 5 to 7
 
 // SPEED TOLERANCE
-#define SPEED_THRESHOLD_UPPER 40
-#define SPEED_THRESHOLD_LOWER 15
+#define SPEED_MINIMUM 0.02
+#define SPEED_THRESHOLD_UPPER 0.8 
+#define SPEED_THRESHOLD_LOWER 0.1
 
 // LINEAR_TRANSITION_TOLERANCE
-#define X_TOLERANCE 1.0
-#define Y_TOLERANCE 1.0
+#define X_TOLERANCE 10.0
+#define Y_TOLERANCE 10.0
 
 // TIME BETWEEN FRAMES
 #define FRAME_TIME 16.5 // these are in millisecond
+
+// TURN AND TRACK FACTORS
+#define TURN_DEGREE 80
+#define TRACK_DEGREE 100
 
 // BAUD
 #define BAUD_RATE 115200
 
 // QUEUE SPECS
-#define QUEUE_SIZE 5
+#define QUEUE_SIZE 25
+
+#define DELAY 0
 
 #include <Pixy2.h>
 #include <AccelStepper.h>
@@ -57,7 +62,7 @@ typedef struct {
   PenPosition* pos;
   PenVelocity* vel;
   
-  PenPosition* prevPositions[QUEUE_SIZE];
+  PenPosition prevPositions[QUEUE_SIZE];
 } PenMotion;
 
 typedef struct {
@@ -69,22 +74,9 @@ typedef struct {
 } PenState;
 
 PenPosition posInit = {0, 0};
-PenPosition posInit1 = {0, 0};
-PenPosition posInit2 = {0, 0};
-PenPosition posInit3 = {0, 0};
-PenPosition posInit4 = {0, 0};
-PenPosition posInit5 = {0, 0};
-PenPosition posInit6 = {0, 0};
-PenPosition posInit7 = {0, 0};
-PenPosition posInit8 = {0, 0};
-PenPosition posInit9 = {0, 0};
-PenPosition posInit10 = {0, 0};
 
 PenVelocity velInit = {0.0, 0.0};
-PenMotion penMotion = {&posInit, &velInit, {&posInit1, &posInit2, &posInit3,
-                                            &posInit4, &posInit5, &posInit6,
-                                            &posInit7, &posInit8, &posInit9,
-                                            &posInit10}};
+PenMotion penMotion = {&posInit, &velInit};
 PenState penState = {false, false, false, false, 'N'};
 int setupCounter = 0;
 int moveCounter = 0;
@@ -98,9 +90,11 @@ void setup() {
 
   yaxis.setMaxSpeed(MAX_SPEED);
   yaxis.setAcceleration(ACCELERATION / 4.0);
+
   pixy.init();
   Serial.begin(BAUD_RATE);
 
+  // enable the drivers, the motors will remain constantly energized
   digitalWrite(STEPPER_ENABLE_PIN, LOW);
 
   move(0, 0, true); // initialize to origin
@@ -108,6 +102,8 @@ void setup() {
   delay(1000);
 }
 
+/// ******* FRONT - END MOTION ******** ////
+/****************************************************************/
 /// Call the run() function for each stepper driver object, which will
 /// recalculate speeds and generate step pulses as needed.
 void poll_steppers(void){
@@ -187,6 +183,7 @@ void skid(int degree) {
       }  
   }
 }
+///// ****** FRONT END MOTION ********** ///////
 
 /* PenState handlers */
 void resetPenState() {
@@ -194,13 +191,13 @@ void resetPenState() {
 }
 
 void decideHorizontal() {
-  uint16_t firstPosition = penMotion.prevPositions[0]->x;
+  uint16_t firstPosition = penMotion.prevPositions[0].y;
   bool isHorizontal = true;
   // check if all positions are in tolerance of the firstPosition
   for (int i = 0; i < QUEUE_SIZE; i += 1) {
-    uint16_t currentPos = penMotion.prevPositions[i]->x;
-    if (currentPos < firstPosition - X_TOLERANCE || 
-        currentPos > firstPosition + X_TOLERANCE) {
+    uint16_t currentPos = penMotion.prevPositions[i].y;
+    if (currentPos < firstPosition - Y_TOLERANCE || 
+        currentPos > firstPosition + Y_TOLERANCE) {
       isHorizontal = false;
       break;  
     } 
@@ -209,13 +206,13 @@ void decideHorizontal() {
 }
 
 void decideVertical() {
-  uint16_t firstPosition = penMotion.prevPositions[0]->y;
+  uint16_t firstPosition = penMotion.prevPositions[0].x;
   bool isVertical = true;
   // check if all positions are in tolerance of the firstPosition
   for (int i = 0; i < QUEUE_SIZE; i += 1) {
-    uint16_t currentPos = penMotion.prevPositions[i]->y;
-    if (currentPos < firstPosition - Y_TOLERANCE || 
-        currentPos > firstPosition + Y_TOLERANCE) {
+    uint16_t currentPos = penMotion.prevPositions[i].x;
+    if (currentPos < firstPosition - X_TOLERANCE || 
+        currentPos > firstPosition + X_TOLERANCE) {
       isVertical = false;
       break;  
     } 
@@ -225,7 +222,7 @@ void decideVertical() {
 
 void decideFast() {
   double penSpeed = sqrt(penMotion.vel->vx * penMotion.vel->vx + penMotion.vel->vy * penMotion.vel->vy);
-  
+
   penState.isFast = (penSpeed > SPEED_THRESHOLD_UPPER);
 }
 
@@ -236,48 +233,48 @@ void decideSlow() {
 }
 
 void decideDirection() {
-  double mag = sqrt(penMotion.vel->vx * penMotion.vel->vx + penMotion.vel->vy * penMotion.vel->vy);
 
+  Serial.println("Deciding direction");
+
+  Serial.print("Velocity Y component: ");
+  Serial.println(penMotion.vel->vy);
+  Serial.print("Velocity X component: ");
+  Serial.println(penMotion.vel->vx);
+  
   double vy_abs = abs(penMotion.vel->vy);
   double vx_abs = abs(penMotion.vel->vx);
 
-  double angle = atan(vy_abs / vx_abs);
+  
+  Serial.print("Velocity Y component (ABS): ");
+  Serial.println(vy_abs);
+  Serial.print("Velocity X component (ABS): ");
+  Serial.println(vx_abs);
 
-  if (penMotion.vel->vx > 0 && penMotion.vel->vy > 0) {
-    // NE  
-    if (angle < PI / 4) {
-      penState.dir = 'E';
+  if (vy_abs > vx_abs) {
+    if (penMotion.vel->vy <= 0.0) {
+      penState.dir = 'S';  
+    } else if (penMotion.vel->vy > 0.0){
+        penState.dir = 'N';  
     } else {
-        penState.dir = 'N';
-    }
-  } else if (penMotion.vel->vx > 0 && penMotion.vel->vy < 0) {
-    // SE
-    if (angle < PI / 4) {
-      penState.dir = 'E';
-    } else {
-        penState.dir = 'S';
-    }
-  } else if (penMotion.vel->vx < 0 && penMotion.vel->vy > 0) {
-    // NW
-    if (angle < PI / 4) {
-      penState.dir = 'W';
-    } else {
-        penState.dir = 'N';
-    }
+        Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (Y)"); 
+     }
+  } else if (vx_abs > vy_abs) {
+       if (penMotion.vel->vx <= 0.0) {
+          penState.dir = 'W';
+       } else if (penMotion.vel->vx > 0.0) {
+          penState.dir = 'E';   
+       } else {
+            Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (X)"); 
+       }
   } else {
-    // SW
-    if (angle < PI / 4) {
-      penState.dir = 'W';
-    } else {
-        penState.dir = 'S';
-    }
+      Serial.println("Direction could not be decided (SHOULD NOT HAPPEN) (ABS)");  
   }
 }
 
 void frameListSetup(uint16_t x, uint16_t y) {
   if (setupCounter < QUEUE_SIZE) {
-    penMotion.prevPositions[setupCounter]->x = x;
-    penMotion.prevPositions[setupCounter]->y = y;
+    penMotion.prevPositions[setupCounter].x = x;
+    penMotion.prevPositions[setupCounter].y = y;
     setupCounter += 1; 
   }
 }
@@ -287,33 +284,33 @@ void frameListSetup(uint16_t x, uint16_t y) {
  */
 void addBlockToFrameList(uint16_t x, uint16_t y) {
     for (int i = 0; i < QUEUE_SIZE - 1; i += 1) {
-      penMotion.prevPositions[i]->x = penMotion.prevPositions[i+1]->x;
-      penMotion.prevPositions[i]->y = penMotion.prevPositions[i+1]->y;
+      penMotion.prevPositions[i].x = penMotion.prevPositions[i+1].x;
+      penMotion.prevPositions[i].y = penMotion.prevPositions[i+1].y;
     }
 
-    penMotion.prevPositions[QUEUE_SIZE - 1]->x = x;
-    penMotion.prevPositions[QUEUE_SIZE - 1]->y = y; 
+    penMotion.prevPositions[QUEUE_SIZE - 1].x = x;
+    penMotion.prevPositions[QUEUE_SIZE - 1].y = y; 
 }
 
 void updatePenPosition() {
-  uint16_t x = penMotion.prevPositions[QUEUE_SIZE - 1]->x;
-  uint16_t y = penMotion.prevPositions[QUEUE_SIZE - 1]->y;
+  uint16_t x = penMotion.prevPositions[QUEUE_SIZE - 1].x;
+  uint16_t y = penMotion.prevPositions[QUEUE_SIZE - 1].y;
 
   penMotion.pos->x = x;
   penMotion.pos->y = y;
 }
 
 void updatePenVelocities() {
-  uint16_t mostRecentX = penMotion.prevPositions[QUEUE_SIZE - 1]->x;
-  uint16_t mostRecentY = penMotion.prevPositions[QUEUE_SIZE - 1]->y;
+  uint16_t mostRecentX = penMotion.prevPositions[QUEUE_SIZE - 1].x;
+  uint16_t mostRecentY = penMotion.prevPositions[QUEUE_SIZE - 1].y;
 
   double velocitySumX = 0.0;
   double velocitySumY = 0.0;
 
   // get average of all velocities
   for (int i = QUEUE_SIZE - 2; i >= 0; i -= 1) {
-    uint16_t x = penMotion.prevPositions[i]->x;
-    uint16_t y = penMotion.prevPositions[i]->y; 
+    uint16_t x = penMotion.prevPositions[i].x;
+    uint16_t y = penMotion.prevPositions[i].y; 
 
     double velocityX = (mostRecentX * 1.0 - x) / ((QUEUE_SIZE - 1 - i) * FRAME_TIME);
     double velocityY = (mostRecentY * 1.0 - y) / ((QUEUE_SIZE - 1 - i) * FRAME_TIME);
@@ -324,6 +321,20 @@ void updatePenVelocities() {
 
   penMotion.vel->vx = velocitySumX / (QUEUE_SIZE - 1);
   penMotion.vel->vy = velocitySumY / (QUEUE_SIZE - 1);
+
+//  if (abs(penMotion.vel->vx) < SPEED_MINIMUM) {
+//    penMotion.vel->vx = 0.0;  
+//  }
+//
+//  if (abs(penMotion.vel->vy < SPEED_MINIMUM)) {
+//    penMotion.vel->vy = 0.0;  
+//  }
+
+//  Serial.print("Velocity X component: ");
+//  Serial.println(penMotion.vel->vx);
+//
+//  Serial.print("Velocity Y component: ");
+//  Serial.println(penMotion.vel->vy);
 }
 
 void decideMove() {
@@ -335,78 +346,100 @@ void decideMove() {
   decideSlow();
   decideDirection();
 
+  if (penState.isHorizontal == true) {
+    Serial.println("Pen is horizontal");  
+  } 
+
+  if (penState.isVertical == true) {
+    Serial.println("Pen is vertical");  
+  } 
+
+  if (penState.isFast == true) {
+    Serial.println("Pen is fast");  
+  }
+
+  if (penState.isSlow == true) {
+    Serial.println("Pen is slow");  
+  }
+
+  // pen direction
+  Serial.print("Pen direction is: ");
+  Serial.println(penState.dir);
+
+//  track(30, 'B');
+
   switch (penState.dir) {
     case 'N':
       // CHECK FOR LINEARITY
       if (penState.isHorizontal) {
-        // turn or jiggle  
-        skid(10);
-      } else if (penState.isVertical) {
         // turn or jiggle 
-        skid(10); 
+        Serial.println("Do nothing. This ideally should not be happening"); 
+      } else if (penState.isVertical) {
+        // turn or jiggle  
+        skid(TURN_DEGREE);
       }
 
       // CHECK FOR SPEED
       if (penState.isFast) {
         // slow down
-        track(50, 'F');
+        track(TRACK_DEGREE, 'B');
       } else if (penState.isSlow) {
         // speed up
-        track(50, 'B');
+        track(TRACK_DEGREE, 'F');
       }
     break;
     case 'E':
       if (penState.isHorizontal) {
-        // turn or jiggle  
-        turnClockwise(30);
+        // turn or jiggle
+        turnClockwise(TURN_DEGREE);  
       } else if (penState.isVertical) {
-        // turn or jiggle  
-        turnClockwise(30);
+        // turn or jiggle 
+        Serial.println("Do nothing. This ideally should not be happening"); 
       }
   
       // CHECK FOR SPEED
       if (penState.isFast) {
-        turnClockwise(60);
         // slow down
+        turnClockwise(TURN_DEGREE);
       } else if (penState.isSlow) {
-        turnAntiClockwise(60);
         // speed up
+        turnAntiClockwise(TURN_DEGREE);
       }
     break;
     case 'S':
       if (penState.isHorizontal) {
         // turn or jiggle  
-        skid(10);
+        Serial.println("Do nothing. This ideally should not be happening"); 
       } else if (penState.isVertical) {
         // turn or jiggle  
-        skid(10);
+        skid(TURN_DEGREE);
       }
 
       // CHECK FOR SPEED
       if (penState.isFast) {
         // slow down
-        track(50, 'B');
+        track(TRACK_DEGREE, 'F');
       } else if (penState.isSlow) {
         // speed up
-        track(50, 'F');
+        track(TRACK_DEGREE, 'B');
       }
     break;
     case 'W':
       if (penState.isHorizontal) {
-        // turn or jiggle 
-        turnAntiClockwise(30); 
+        // turn or jiggle  
+        turnAntiClockwise(TURN_DEGREE);
       } else if (penState.isVertical) {
-        // turn or jiggle 
-        turnAntiClockwise(30); 
+        // turn or jiggle  
+        Serial.println("Do nothing. This ideally should not be happening");
       }
 
       // CHECK FOR SPEED
       if (penState.isFast) {
         // slow down
-        turnAntiClockwise(60);
+        turnAntiClockwise(TURN_DEGREE);
       } else if (penState.isSlow) {
         // speed up
-        turnClockwise(60);
+        turnClockwise(TURN_DEGREE);
       }
     break;  
     default:
@@ -414,17 +447,36 @@ void decideMove() {
   }
 }
 
+void printFrameList() {
+  for (int i = QUEUE_SIZE - 1; i >= 0; i -= 1) {
+    Serial.print("Frame #: ");
+    Serial.println(i + 1);
+
+    Serial.print("X position: ");
+    Serial.println(penMotion.prevPositions[i].x);
+    Serial.print("Y position: ");
+    Serial.println(penMotion.prevPositions[i].y);
+  }  
+}
+
 void loop() {
+  //turnClockwise(60);
+  //track(60, 'B');
+  //skid(60);
 //  Serial.println(penMotion.pos->x);
+  //Serial.println("Getting blocks");
   pixy.ccc.getBlocks();
+  //Serial.println("Got blocks");
   // If there are detect blocks, print them!
   if (pixy.ccc.numBlocks) {
-    uint16_t x = pixy.ccc.blocks[0].m_x;
-    uint16_t y = pixy.ccc.blocks[0].m_y;
+    uint16_t x = pixy.ccc.blocks[0].m_x * X_DISTANCE_FACTOR;
+    uint16_t y = pixy.ccc.blocks[0].m_y * Y_DISTANCE_FACTOR;
 
-    Serial.println(pixy.ccc.blocks[0].m_x); // only take most relevant block
-    Serial.print("Y value = ");
-    Serial.println(pixy.ccc.blocks[0].m_y);
+
+//    Serial.print("X value = ");
+//    Serial.println(pixy.ccc.blocks[0].m_x); // only take most relevant block
+//    Serial.print("Y value = ");
+//    Serial.println(pixy.ccc.blocks[0].m_y);
 
     if (setupCounter < QUEUE_SIZE) {
       frameListSetup(x, y);  
@@ -435,12 +487,14 @@ void loop() {
       
         updatePenVelocities();
 
-        if (moveCounter % 10 == 0) {
+        if (moveCounter % QUEUE_SIZE == 0) {
+          //printFrameList();
           decideMove();
-          moveCounter = 0;  
+          moveCounter = 0; 
+          delay(DELAY); 
         }
 
-        moveCounter += 1;  
+        moveCounter += 1;
     }
   }  
 }
